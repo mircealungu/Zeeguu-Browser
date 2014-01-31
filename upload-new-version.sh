9269a6969a96f0e367e8f4c8560cd0e5038c2c94
Mircea Lungu <mircea.lungu@gmail.com>
2014-01-31 08:00:32 +0100

    rephrased

--- upload-new-version.sh
+++ upload-new-version.sh
@@ -2,5 +2,5 @@
 python increase_version_no.py
 zip -r zeeguu.zip * >/dev/null
 
-read -p "Press any key to start uploading..."
+read -p "Press any key for the Developer Dashboard..."
 open https://chrome.google.com/webstore/developer/edit/ckncjmaednfephhbpeookmknhmjjodcd
