0977e29a217c8cfb1d758718c94219789244bfe8
Mircea Lungu <mircea.lungu@gmail.com>
2015-02-03 19:29:22 +0100

    playing a bit with the style of the popup

--- inject/js/translation_overlay.js
+++ inject/js/translation_overlay.js
@@ -38,11 +38,11 @@ function mouse_up_in_page(e, external_dictionary_active) {
         function update_bubble_with_translation(translation) {
             if (translation) {
 
-                var more = document.createElement('span');
+                var more = document.createElement('div');
                 if (document.URL.lastIndexOf("https", 0) != 0) {
-                    more.style.cssText = "margin-left: 16px; float: right; color: gray;";
-                    more.className= "translation-popup-link";
-                    more.innerHTML = ' <a href="javascript:void(0)">...</a> ';
+                    more.style.cssText = "font-size: 10px; padding-left: 0px; clear:both; float: left; color: gray;";
+                    more.className= "translation-popup-more";
+                    more.innerHTML = ' <a href="javascript:void(0)"><i class="fa fa-pencil"></i>Wrong?</a> ';
                     more.addEventListener('mouseup', function (ev) {
                         /*
                          I guess here we must send a message from
@@ -59,17 +59,31 @@ function mouse_up_in_page(e, external_dictionary_active) {
                     });
                 }
 
-                var save = document.createElement('span');
-//                            save.style.cssText="float:left";
-                save.className= "translation-popup-link";
-                save.innerHTML = '<a href="javascript:void(0)"><i class="fa fa-cloud-upload"></i></a> ';
-                save.addEventListener('mouseup', function (ev) {
+                var bookmark = document.createElement('div');
+                bookmark.id = "translation-popup-bookmark";
+                bookmark.className= "translation-popup-bookmark";
+                bookmark.style.cssText="float:right; margin-bottom: 10px; margin-left:5px;";
+                bookmark.innerHTML = '<a href="javascript:void(0)"><i class="fa fa-bookmark-o"></i></a> ';
+
+
+                $(bookmark).hover(
+                    function() {
+                        $(this).find($(".fa")).removeClass('fa-bookmark-o').addClass('fa-bookmark');
+                    },
+                    function () {
+                        $(this).find($(".fa")).removeClass('fa-bookmark').addClass('fa-bookmark-o');
+                    }
+                );
+
+
+                bookmark.addEventListener('mouseup', function (ev) {
                     /*
                      I guess here we must send a message from
                      the page that will be intercepted by the
                      plugin to open the full dictionary if one
                      exists...
                      */
+
                     translationOverlay.style.visibility = 'hidden';
 
                     var script = document.createElement("script");
@@ -82,7 +96,7 @@ function mouse_up_in_page(e, external_dictionary_active) {
                     document.body.appendChild(script);
                 });
 
-                var close = document.createElement('span');
+                var close = document.createElement('div');
                 close.innerHTML = " (close)";
                 close.addEventListener('mousedown', function (ev) {
                     translationOverlay.style.visibility = 'hidden';
@@ -99,12 +113,12 @@ function mouse_up_in_page(e, external_dictionary_active) {
 
                 translationOverlay.innerHTML = "";
 
-                var translation_span = document.createElement('div');
-                translation_span.style.cssText="text-align: center; margin-bottom: 10px;";
-                translation_span.innerHTML = "<b>" + translation + "</b>";
+                var translation_span = document.createElement('span');
+                translation_span.style.cssText="float:left; margin-bottom: 10px;";
+                translation_span.innerHTML = " <b>" + translation + "</b> ";
 
                 translationOverlay.appendChild(translation_span);
-                translationOverlay.appendChild(save);
+                translationOverlay.appendChild(bookmark);
                 translationOverlay.appendChild(more);
 
 
